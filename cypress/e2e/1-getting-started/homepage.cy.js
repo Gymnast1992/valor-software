16ba2bc17ade92ea41fc4b103b2fe1e39ccc42e8
Maksym Semiankiv <1992maksym1992@gmail.com>
2025-01-04 20:25:00 +0100

    4 simple test cases

--- cypress/e2e/1-getting-started/homepage.cy.js
+++ cypress/e2e/1-getting-started/homepage.cy.js
@@ -1,5 +1,5 @@
 const text = "We are not coders, we're true engineers and specialized experts with a deep understanding of languages and frameworks necessary to build the next-gen apps and platforms.";
-
+const supportEmail = "support@valor-software.com"
 describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
@@ -14,11 +14,22 @@ describe("Homepage", () => {
 
   it("Should verify the lenght of the pages in the side-bar", () => {
     cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
-    cy.get(`[class="_long-link_19pvy_30"]`).should("have.length", 8);
+    cy.get('[class="_long-link_19pvy_30"]').should("have.length", 8);
   });
 
   it("Should scroll down to the bottom and verify the text", () => {
-    
+    cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
+    cy.scrollTo('bottom');
+    cy.contains(supportEmail).should("be.visible");
+  });
+
+  it.only("Should navigate to the Blog section and verify the first listing", () => {
+    cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
+    cy.wait(2000);
+    cy.get(".hamburger-react").click({force:true});
+    cy.get("header ul li").should("have.length", 7);
+    cy.get("header ul li").eq(6).click();
+    cy.get("._list-block_1eio8_14 > :nth-child(1)").should("be.visible");
   });
 
 });
