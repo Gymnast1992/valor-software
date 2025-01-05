e29de30042cdf525eb6b34fcadb578c5b2ee240c
Maksym Semiankiv <1992maksym1992@gmail.com>
2025-01-05 18:14:48 +0100

    5 test cases + ci.yaml + .gitignore

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,37 +1,35 @@
 import HomePage from "../fixtures/pom/homepage";
-const text = "We are not coders, we're true engineers and specialized experts with a deep understanding of languages and frameworks necessary to build the next-gen apps and platforms.";
+const text =
+  "We are not coders, we're true engineers and specialized experts with a deep understanding of languages and frameworks necessary to build the next-gen apps and platforms.";
 const supportEmail = "support@valor-software.com";
 const name = "Dima Shekhovtsov, GDE";
-
 
 describe("Homepage", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
-  it("TC01, Verifying the text and the name on the homepage", () => {
+  it("TC01, Should verify the text and the name on the homepage", () => {
     HomePage.verifyTheText(text);
     HomePage.verifyTheName(name);
   });
 
-  it("Should verify the lenght of the pages in the side-bar", () => {
-    cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
-    cy.get('[class="_long-link_19pvy_30"]').should("have.length", 8);
+  it("TC02, Should verify the length of side-bar pages", () => {
+    HomePage.verifyPagesLength(8);
   });
 
-  it("Should scroll down to the bottom and verify the text", () => {
-    cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
-    cy.scrollTo('bottom');
-    cy.contains(supportEmail).should("be.visible");
+  it("TC03, Should scroll down to the bottom and verify the support email", () => {
+    HomePage.scrollToTheBottom;
+    HomePage.supportEmailIsVisible(supportEmail);
   });
 
-  it("Should navigate to the Blog section and verify the first listing", () => {
-    cy.get("._buttons_tof5b_16 > :nth-child(3)").click();
-    cy.wait(2000);
-    cy.get(".hamburger-react").click({force:true});
-    cy.get("header ul li").should("have.length", 7);
-    cy.get("header ul li").eq(6).click();
-    cy.get("._list-block_1eio8_14 > :nth-child(1)").should("be.visible");
+  it("TC04, Should navigate to the Blog section and verify the first listing", () => {
+    HomePage.clickOnTheHumburgerMenu();
+    HomePage.clickOnTheBtnBlog();
+    HomePage.isFirstBlogListingVisible();
   });
 
+  it("TC05, Should verify the length of the listings in the header", () => {
+    HomePage.verifyTheHeaderLiLength(7);
+  });
 });
--- cypress/fixtures/pom/homepage.js
+++ cypress/fixtures/pom/homepage.js
@@ -1,6 +1,15 @@
+// const supportEmail = "support@valor-software.com";
+
 class HomePage {
     get subtitle() {return cy.get(".subtitle")};
     get imageContainer() {return cy.get('[class="_image-container_6splr_30"]')};
+    get sideBarPages() {return cy.get('[class="_long-link_19pvy_30"]')};
+    get scrollToTheBottom() {return cy.scrollTo('bottom')};
+    get supportEmail() {return cy.contains(email)};
+    get humburgerMenu() {return cy.get(".hamburger-react")};
+    get btnBlog() {return cy.contains("Blog")};
+    get firstBlogListing() {return cy.get("._list-block_1eio8_14 > :nth-child(1)")};
+    get headerSection() {return cy.get("header ul li")};
 
     verifyTheText(text){
         this.subtitle.should("have.text", text);
@@ -9,6 +18,31 @@ class HomePage {
     verifyTheName(name) {
         this.imageContainer.should("contain.text", name);
     }
+
+    verifyPagesLength(length){
+        this.sideBarPages.should("have.length", length);
+    }
+
+    supportEmailIsVisible(email) {
+        cy.contains(email).should("be.visible");
+    }
+
+    clickOnTheHumburgerMenu() {
+        this.humburgerMenu.click({force:true});
+    }
+
+    clickOnTheBtnBlog() {
+        this.btnBlog.click();
+    }
+
+    isFirstBlogListingVisible() {
+        this.firstBlogListing.should("be.visible");
+    }
+
+    verifyTheHeaderLiLength(length) {
+        this.headerSection.should("have.length", length)
+    }
+
 }
 
 export default new HomePage()
